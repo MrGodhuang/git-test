31f1a72d3d129041b39a99a597e87a5ee63cca6d
Gangyi <547449293@qq.com>
2021-08-16 09:10:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Gangyi Huang
+ * @create 2021-08-16 8:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello! Git!");
+    }
+}
